a8654f9ff4c0d76d181269cd04ff64df30c5bdd9
sudodoki <smd.deluzion@gmail.com>
2014-12-21 00:28:11 +0200

    Add express & hello world server

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log('Hey!');
+var express = require('express');
+
+var app = express();
+
+var port = 8080;
+
+app.get('*', function (req, res) {
+  res.end('Hello world!');
+});
+
+app.listen(port);
+console.log('App is running on %s port', port);
