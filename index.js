79fad48fb74cc30aa697d57a847b5a18203f7db1
sudodoki <smd.deluzion@gmail.com>
2014-12-21 02:38:51 +0200

    Switch local strategy for github login

--- index.js
+++ index.js
@@ -3,44 +3,39 @@
 var express = require('express'),
     path = require('path'),
     passport = require('passport'),
-    LocalStrategy = require('passport-local'),
+    GitHubStrategy = require('passport-github').Strategy,
     cookieParser = require('cookie-parser'),
     session = require('express-session'),
     LeveldbStore = require('connect-leveldb')(session),
     app = express(),
     bodyParser = require('body-parser'),
     port = 8080,
-    HARDCODED_USERS;
-
-HARDCODED_USERS = [
-  { id: 1, username: 'admin', password: 'admin', email: 'admin@this.shit' }
-];
+    GITHUB_CLIENT_ID = '14c495435d08a41b588d',
+    GITHUB_CLIENT_SECRET = 'eb9aea97ab08eb9a22f9b94900770c2beaf434be';
 
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  var user = HARDCODED_USERS.filter(function(u) { return u.id === id; });
-  if (!user) { return done(new Error('User does not exist')); }
   done(null, user);
 });
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    var user = HARDCODED_USERS.filter(function(u) {
-      return u.username === username;
-    })[0];
-    if (user && user.password === password) {
-      return process.nextTick(function() {
-        done(null, user);
-      });
-    }
-    return process.nextTick(function() {
-      done(null, false);
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GitHubStrategy({
+  clientID: GITHUB_CLIENT_ID,
+  clientSecret: GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:8080/auth/github/callback'
+},
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function() {
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
     });
-  }
-));
+  }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
@@ -63,25 +58,33 @@ app.get('/login', function(req, res) {
   res.render('login.html', {user: req.user });
 });
 
-app.get('/logout', function (req, res) {
+app.get('/auth/github',
+  passport.authenticate('github'),
+  function(req, res) {
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  });
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res) {
   req.logout();
   console.log('loggint out');
   res.redirect('/');
 });
 
-app.post('/login', function(req, res, next) {
-    console.log(req.body);
-    next();
-  },
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
-
 app.get('/',
   ensureAuthenticated,
   function(req, res) {
   res.end('Hello world!');
+});
+
+app.get('/account', ensureAuthenticated, function(req, res) {
+  res.render('user.html', {user: req.user});
 });
 
 app.listen(port);
